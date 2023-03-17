bb37303dfd6fa5bb97dc059172404fd396f3d612
Ock Ju Won <ockjuwon99@gmail.com>
2023-03-17 17:38:34 +0900

    Add & style onTimer

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 const gameBtn = document.querySelector('.gameBtn');
-const GAME_TIME = 60000; // 60seconds for game
+const timer = document.querySelector('.timer');
+const gameStage = document.querySelector('.gameStage');
+
+const carrot = '\src\img\carrot.png';
+const bug = '\src\img\bug.png';
+const MAX_HEIGHT = Math.floor(window.innerHeight / 2);
+let TIME_SEC = 59;
 
 function isGameOver() {
     // Game over conditions:
     // 1. when the player kills all the bugs => you win!
     
     // 2. when the player kills any carrot => you lose!
+}
 
-    // 3. when timeout => you lose!
-    setTimeout(() => {
-        alert("Game over!");
-        window.location.reload();
-    }, GAME_TIME); 
+// // Until 10, generate random bug and carrot generating numbers
+// function generateNumber() {
+//     const bugNum = Math.floor(Math.random() * (max - min + 1) + min);
+
+// }
+
+function onTimer() {
+  const intervalId = setInterval(() => {
+    const timeShow = TIME_SEC % 60;
+    TIME_SEC--;
+    // Update timer
+    timer.innerHTML = `<span>${timeShow}</span>`;
+    // When timer is done, pause intervalId with clearInterval
+    if (TIME_SEC < 0) {
+      clearInterval(intervalId);
+      window.location.reload();
+      alert("Game over!");
+    }}, 1000);
 }
 
 function gameStart(e) {
     // Game start!
     gameBtn.classList.add('hidden');
+    timer.classList.remove('hidden');
+    onTimer();
     isGameOver();
 }
 
