16cc1e117a7169a007cc09ca616a604e00ea7468
Ock Ju Won <ockjuwon99@gmail.com>
2023-03-17 16:16:21 +0900

    Define game over conditions

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const gameBtn = document.querySelector('.gameBtn');
+const GAME_TIME = 60000; // 60seconds for game
+
+function isGameOver() {
+    // Game over conditions:
+    // 1. when the player kills all the bugs => you win!
+    
+    // 2. when the player kills any carrot => you lose!
+
+    // 3. when timeout => you lose!
+    setTimeout(() => {
+        alert("Game over!");
+        window.location.reload();
+    }, GAME_TIME); 
+}
 
 function gameStart(e) {
-    console.log('hi');
+    // Game start!
+    gameBtn.classList.add('hidden');
+    isGameOver();
 }
 
 gameBtn.addEventListener('click', gameStart);
